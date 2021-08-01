475e1846738b950e0c352cc7ed50fcbded6b3325
zhangruyi <156980742@qq.com>
2021-08-01 17:09:36 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{ test: /\.ts$/, use: [{
+      loader: 'ts-loader',
+      options: {
+        happyPackMode: true,
+        transpileOnly: true,
+        compilerOptions: {
+          noEmit: false,
+          module: 'esnext',
+          target: 'es2017',
+        },
+      },
+    }]}],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+  devtool: 'inline-source-map',
+};
